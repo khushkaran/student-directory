9e872606e8c98fb065e9c117eb780b30dd893102
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-02-21 16:44:06 +0000

    Fixed bug: nothing was printed when no cohort was entered

--- directory.rb
+++ directory.rb
@@ -4,6 +4,10 @@ def print_s(length)
 	length == 1 ? "" : "s"
 end
 
+def cohort_entered(cohort)
+	cohort == "" ? "Unknown" : cohort
+end
+
 def input_students
 	cohort_entry_string = "Please enter the cohort"
 	puts "Please enter a name"
@@ -17,7 +21,7 @@ def input_students
 		puts cohort_entry_string
 		cohort = gets.chomp
 		# Add the student name & cohort to the array
-		students << {:name => name, :cohort => cohort = cohort ||= "Unknown"}
+		students << {:name => name, :cohort => cohort_entered(cohort)}
 		# Get another email from the user
 		puts "We currently have #{students.length} student#{print_s(students.length)}, please enter another or press enter to quit!"
 		name = gets.chomp
